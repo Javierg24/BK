6aea8a7b446729bd9a18da6a405655562e27b71a
Javierg24 <javierruiz.24@campuscamara.es>
2024-12-04 11:28:26 +0100

    a
    a

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -17,40 +17,40 @@ export class MenuComponent implements OnInit {
       nombre: "Novedades",
       foto: '../../assets/img/carta/novedades.png',
       productos: [
-        { nombre: "MENÚ KING DABIZ II CARNE", imagen: '../../assets/img/productos/' },
-        { nombre: "GOUDA RINGS", imagen: '../../assets/img/productos/' }
+        { nombre: "MENÚ KING DABIZ II CARNE", imagen: '../../assets/img/productos/Burguirs.png' },
+        { nombre: "GOUDA RINGS", imagen: '../../assets/img/productos/GoudaRings.png' }
       ]
     },
     {
       nombre: "Menú Parrilla",
       foto: '../../assets/img/carta/whopper2.png',
       productos: [
-        { nombre: "Whopper", imagen: '../../assets/img/productos/whopper2.png' },
-        { nombre: "Double Whopper", imagen: '../../assets/img/productos/dobleWhopper.jpg' }
+        { nombre: "WHOPPER", imagen: '../../assets/img/productos/whopper2.png' },
+        { nombre: "DOBLE WHOPPER", imagen: '../../assets/img/productos/dobleWhopper.jpg' }
       ]
     },
     {
       nombre: "Menú de Pollo",
       foto: '../../assets/img/carta/pollo.png',
       productos: [
-        { nombre: "Crispy Chicken", imagen: '../../assets/img/productos/'},
-        { nombre: "Chicken Royale", imagen: '../../assets/img/productos/'}
+        { nombre: "MENÚ CRISPY CHICKEN", imagen: '../../assets/img/productos/pollo.png' },
+        { nombre: "Menú LONG CHICKEN", imagen: '../../assets/img/productos/LongChicken.png' }
       ]
     },
     {
       nombre: "King Jr",
       foto: '../../assets/img/carta/KingJR2.png',
       productos: [
-        { nombre: "Hamburguesa King Jr", imagen: '../../assets/img/productos/' },
-        { nombre: "Nuggets King Jr", imagen: '../../assets/img/productos/' }
+        { nombre: "HAMBURGEUSA KING JR", imagen: '../../assets/img/productos/BurguerKingJR.png' },
+        { nombre: "NUGGETS KING JR", imagen: '../../assets/img/productos/NuggetsKingJR.png' }
       ]
     },
     {
       nombre: "100% Vegetariano",
       foto: '../../assets/img/carta/vegano.png',
       productos: [
-        { nombre: "Plant-Based Whopper", imagen: '../../assets/img/productos/'},
-        { nombre: "Vegan Royale", imagen: '../../assets/img/productos/' }
+        { nombre: "WHOPPER VEGETAL", imagen: '../../assets/img/productos/whopperVegi.png'},
+        { nombre: "MENÚ LONG VEGETAL", imagen: '../../assets/img/productos/MenuLongVegetal.png' }
       ]
     },
     {
@@ -145,4 +145,35 @@ export class MenuComponent implements OnInit {
     const categoria = this.categorias.find(cat => cat.nombre === nombreCategoria);
     this.productos = categoria ? categoria.productos : [];
   }
+
+  @ViewChild('categoriasWrapper') categoriasWrapper!: ElementRef;
+
+  isDragging = false;
+  startX = 0;
+  scrollLeft = 0;
+
+  onMouseDown(event: MouseEvent | TouchEvent): void {
+    this.isDragging = true;
+    const pageX = event instanceof MouseEvent ? event.pageX : event.touches[0].pageX;
+    this.startX = pageX - this.categoriasWrapper.nativeElement.offsetLeft;
+    this.scrollLeft = this.categoriasWrapper.nativeElement.scrollLeft;
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (!this.isDragging) return;
+    event.preventDefault();
+    const x = event.pageX - this.categoriasWrapper.nativeElement.offsetLeft;
+    const walk = (x - this.startX) * 3; // Reduce la velocidad ajustando el multiplicador
+    this.categoriasWrapper.nativeElement.scrollLeft = this.scrollLeft - walk;
+  }
+
+
+  onMouseUp(): void {
+    this.isDragging = false;
+  }
+
+  onMouseLeave(): void {
+    this.isDragging = false;
+  }
+
 }
